54303a4995e26e21a6b3e1e9685dabfc704c576d
webserg <webserg@gmail.com>
2017-09-30 12:16:14 +0300

    docker improve

--- travel9/warmUp.sh
+++ travel9/warmUp.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-for i in {1..1000}
+for i in {1..700}
 do
+   curl localhost/users/$i > out.log 2> err.log
+   curl localhost/visits/$i > out.log 2> err.log
+   curl localhost/locations/$i > out.log 2> err.log
    curl localhost/users/$i/visits > out.log 2> err.log
+   curl localhost/locations/$i/avg > out.log 2> err.log
 done
